d644c1ef61d472a4427b5b0159267cc5d04004c5
NGustavo011 <gsn181200@gmail.com>
2021-10-25 16:21:34 -0300

    start of home page

---
+++ src/pages/Home/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import * as S from './styles'
+
+const Home = () =>{
+    return (
+        <S.Wrapper>
+            <S.Content>
+                <S.Title>
+                    ENCURTADOR DE URL NG
+                </S.Title>
+                <S.Description>
+                    Encurtador de URL NG é uma ferramenta poderosa para encurtar links. 
+                    Use o Encurtador de Link para diminuir uma URL longa e gerar um link curto. Encurte
+                    o link que você quiser.
+                </S.Description>
+            </S.Content>
+        </S.Wrapper>
+    )
+} 
+
+export default Home;
---
+++ src/pages/Home/styles.js
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+`
+
+export const Content = styled.div`
+    max-width: 50%;
+`
+
+export const Title = styled.h1`
+    text-align: center;
+    font-size: 50px;
+`
+
+export const Description = styled.h2`
+    
+`
